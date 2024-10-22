955c7d9c9b64ee5c886ff55b44bd8283eec30597
rachit44 <rachitgupta55555@gmail.com>
2024-10-23 01:08:59 +0530

    halfway towards goal

--- videoUpload/models.py
+++ videoUpload/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    caption = models.CharField(max_length=50)
+    video = models.FileField(upload_to="video/%y")
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.caption
--- videoUpload/views.py
+++ videoUpload/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Video
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    if request.method == "GET":
+        videos = Video.objects.all()
+        return render(request, 'index.html', {'videos':videos})
+    elif request.method == "POST":
+        data = Video(request.POST)
+        print("video is uploaded")
+        print(data)
+        #video = Video.objects.create()
+    
