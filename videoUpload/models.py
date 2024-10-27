3904e4705165b600a08a0eb7e6502aca1595be94
rachit44 <rachitgupta55555@gmail.com>
2024-10-28 01:26:15 +0530

    add functionality to upload and show all videos in nice frame

--- videoUpload/models.py
+++ videoUpload/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Video(models.Model):
     caption = models.CharField(max_length=50)
     video = models.FileField(upload_to="video/%y")
-    description = models.TextField(max_length=1000)
+    description = models.TextField(max_length=1000, null=True)
 
     def __str__(self):
         return self.caption
--- videoUpload/views.py
+++ videoUpload/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Video
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -7,8 +8,17 @@ def home(request):
         videos = Video.objects.all()
         return render(request, 'index.html', {'videos':videos})
     elif request.method == "POST":
-        data = Video(request.POST)
-        print("video is uploaded")
-        print(data)
+        print(request.POST)
+        print(request.FILES)
+        video_file = request.FILES.get("video")
+        if video_file:
+            print("video is uploaded")
+            Video.objects.create(video = video_file, caption=request.POST.get("caption"), description=request.POST.get("description"))
+        return redirect("/home/")
         #video = Video.objects.create()
     
+
+def delete_video(request):
+    if request.method =="DELETE":
+        if request.POST.get("caption"):
+            pass
